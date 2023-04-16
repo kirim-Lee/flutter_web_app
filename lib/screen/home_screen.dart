17fd87566624983c22b87ad219bb3ec016e495ae
이기림(Kirim Lee)/Biz플랫폼개발팀/SKP <kirim@sk.com>
2023-04-16 17:48:00 +0900

    앱뷰 아이콘 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+String homepage = "https://blog.codefactory.ai";
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  WebViewController? _controller; // 컨트롤러
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
         appBar: AppBar(
           backgroundColor: const Color(0xFFFFFF88),
@@ -22,8 +28,22 @@ class HomeScreen extends StatelessWidget {
               ]
           ),
           centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  _controller?.loadUrl(homepage);
+                },
+                icon: const Icon(Icons.home, color: Colors.green,),
+            )
+          ],
         ),
-        body: const WebView(javascriptMode: JavascriptMode.unrestricted, initialUrl: "https://blog.codefactory.ai")
+        body: WebView(
+            onWebViewCreated: (WebViewController controller) {
+              _controller = controller;
+            },
+            javascriptMode: JavascriptMode.unrestricted,
+            initialUrl: homepage,
+        )
     ) ;
   }
 
